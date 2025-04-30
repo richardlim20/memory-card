fee6fe69007eb00cfb4ca6ce899898ad34a69d68
Richard Lim <s3901013@student.rmit.edu.au>
2025-04-30 18:25:16 +1000

    feat: api call

--- src/components/Gamecard.tsx
+++ src/components/Gamecard.tsx
@@ -1,4 +1,10 @@
+import { Pokedex } from "pokeapi-js-wrapper"
 const Gamecard = () => {
+  const P = new Pokedex();
+  (async () => {
+    const golduck = await P.getPokemonByName("golduck")
+    console.log(golduck)
+  })()
   return(
     <>
     <div>Gamecard</div>

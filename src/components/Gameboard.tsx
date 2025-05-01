449232f7fb87fd1cf0f32225eaddacdd06c50388
Richard Lim <s3901013@student.rmit.edu.au>
2025-05-01 16:49:25 +1000

    refactor: extract fetch to gameboard

--- src/components/Gameboard.tsx
+++ src/components/Gameboard.tsx
@@ -1,17 +1,47 @@
-import Gamecard from "./Gamecard"
+import { Pokedex } from "pokeapi-js-wrapper";
+import { useState, useEffect } from "react";
+import Gamecard from "./Gamecard";
 const Gameboard = () => {
   // TODO: Move fetch function into Gameboard
-  return(
-    <>
-    <Gamecard pokemonName={"golduck"}></Gamecard>
-    <Gamecard pokemonName={"psyduck"}></Gamecard>
-    <Gamecard pokemonName={"charmander"}></Gamecard>
-    <Gamecard pokemonName={"squirtle"}></Gamecard>
-    <Gamecard pokemonName={"bulbasaur"}></Gamecard>
-    <Gamecard pokemonName={"pikachu"}></Gamecard>
-    <div>Gameboard</div>
-    </>
-  )
-}
+  const [pokemonData, setPokemonData] = useState();
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      const P = new Pokedex();
+      const pokemonNames = [
+        "golduck",
+        "psyduck",
+        "charmander",
+        "squirtle",
+        "bulbasaur",
+        "pikachu",
+      ];
+      const data = [];
+      for (const pokemonName of pokemonNames) {
+        const pokemon = await P.getPokemonByName(pokemonName);
+        const pokemonSprite = pokemon.sprites.front_default;
+        data.push({ name: pokemonName, sprite: pokemonSprite });
+      }
+      setPokemonData(data);
+    };
+    fetchPokemonData();
+  }, []);
 
-export default Gameboard
+  return (
+    <>
+      {!pokemonData ? (
+        <div>Loading...</div>
+      ) : (
+        pokemonData.map((pokemon) => (
+          <Gamecard
+            key={pokemon.name}
+            pokemonName={pokemon.name}
+            pokemonSprite={pokemon.sprite}
+          />
+        ))
+      )}
+      <div>Gameboard</div>
+    </>
+  );
+};
+
+export default Gameboard;
--- src/components/Gamecard.tsx
+++ src/components/Gamecard.tsx
@@ -1,22 +1,8 @@
-import { Pokedex } from "pokeapi-js-wrapper";
-import { useState } from "react";
-const Gamecard = ({ pokemonName }: {pokemonName: string}) => {
-    // TODO: Move fetch function into Gameboard
-  const [pokemonSprite, setPokemonSprite] = useState<string | null>();
-  const fetchPokemonImg = async () => {
-    const P = new Pokedex();
-    const pokemon = await P.getPokemonByName(pokemonName);
-    setPokemonSprite(pokemon.sprites.front_default);
-  };
-  fetchPokemonImg();
+const Gamecard = ({ pokemonName, pokemonSprite }: {pokemonName: string; pokemonSprite: string;}) => {
   return (
     <>
       <div>Gamecard</div>
-      {!pokemonSprite ? (
-        <div>Loading...</div>
-      ) : (
         <img src={pokemonSprite} alt={pokemonName}></img>
-      )}
     </>
   );
 };

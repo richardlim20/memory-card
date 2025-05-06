2dee7ba20f8eae469240b40134565d08cdadf696
Richard Lim <s3901013@student.rmit.edu.au>
2025-05-06 14:29:12 +1000

    refactor: remove scoreboard.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import './App.css'
-import Scoreboard from './components/Scoreboard'
 import Gameboard from './components/Gameboard'
 
 function App() {
 
   return (
     <>
-    <Scoreboard></Scoreboard>
     <Gameboard></Gameboard>
     </>
   )

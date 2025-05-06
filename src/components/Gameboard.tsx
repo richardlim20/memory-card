b94d44e18ac12bfcad85d05e03d31988a9d67e10
Richard Lim <s3901013@student.rmit.edu.au>
2025-05-06 14:26:36 +1000

    feat: score tracking

--- src/components/Gameboard.tsx
+++ src/components/Gameboard.tsx
@@ -3,14 +3,9 @@ import { useState, useEffect } from "react";
 import Gamecard from "./Gamecard";
 const Gameboard = () => {
   const [pokemonData, setPokemonData] = useState();
-
-  //Shuffle array elements function to randomise placement
-  const shuffle = (array: string[]) => {
-    return array
-      .map((a) => ({ sort: Math.random(), value: a }))
-      .sort((a, b) => a.sort - b.sort)
-      .map((a) => a.value);
-  };
+  const [clickedPokemon, setClickedPokemon] = useState<string[]>([]);
+  const [score, setScore] = useState<number>(0);
+  const [highscore, setHighscore] = useState<number>(0);
 
   useEffect(() => {
     const fetchPokemonData = async () => {
@@ -26,7 +21,7 @@ const Gameboard = () => {
       ];
       const P = new Pokedex();
       const data = [];
-      const shuffledPokemon = shuffle(pokemonNames)
+      const shuffledPokemon = shuffle(pokemonNames);
       for (const pokemonName of shuffledPokemon) {
         const pokemon = await P.getPokemonByName(pokemonName);
         const pokemonSprite = pokemon.sprites.front_default;
@@ -37,14 +32,49 @@ const Gameboard = () => {
     fetchPokemonData();
   }, []);
 
+  //Shuffle array elements function to randomise placement
+  const shuffle = (array: string[]) => {
+    return array
+      .map((a) => ({ sort: Math.random(), value: a }))
+      .sort((a, b) => a.sort - b.sort)
+      .map((a) => a.value);
+  };
+
   const shufflePokemon = () => {
     if (pokemonData) {
       setPokemonData(shuffle(pokemonData));
     }
-  }
+  };
+
+  const calculateHighscore = (score) => {
+    if (highscore < score) {
+      setHighscore(score);
+    }
+    return highscore;
+  };
+
+  const handlePokemonClick = (pokemonName: string) => {
+    if (clickedPokemon.includes(pokemonName)) {
+      setScore(0);
+      setClickedPokemon([]);
+    } else {
+      clickedPokemon.push(pokemonName);
+      setScore(clickedPokemon.length);
+    }
+    shufflePokemon();
+  };
+
+  const restartGame = () => {
+    setClickedPokemon([]);
+    setScore(0);
+    setHighscore(0);
+    shufflePokemon();
+  };
 
   return (
     <>
+      <div>Score: {score}</div>
+      <div>Highscore: {calculateHighscore(score)}</div>
       <div id="gameboard">
         <div id="card-container">
           {!pokemonData ? (
@@ -55,12 +85,12 @@ const Gameboard = () => {
                 key={pokemon.name}
                 pokemonName={pokemon.name}
                 pokemonSprite={pokemon.sprite}
-                onClick={shufflePokemon}
+                onClick={() => handlePokemonClick(pokemon.name)}
               />
             ))
           )}
         </div>
-        <button onClick={shufflePokemon}>Restart Game</button>
+        <button onClick={restartGame}>Restart Game</button>
       </div>
     </>
   );
